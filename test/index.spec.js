dacf0eeef0e2f46450b5f2c14a67913395b2a3f6
williamlacy <williamlacy@ucds.email>
2017-11-21 16:47:18 +0000

    basic lint functionality

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,7 +3,7 @@ const { describe, it, beforeEach } = require('mocha');
 const LintTargetBlank = require('./../index');
 let underTest;
 const path = require('path');
-const getFixture = fixture => path.join(__dirname, './fixtures', 'page-without-both-rel.html');
+const getFixture = fixture => path.join(__dirname, './fixtures', fixture);
 const expectedResult = (href, rel=null) => ({
   element: `<a href=\"${href}\" target=\"_blank\"${rel !== null ? ` rel=\"${rel}\"`: '' }></a>`,
   type: 'error'
@@ -26,16 +26,31 @@ describe('lint-target-blank', ()=>{
       ]))
   );
   it('should identify links where target="_blank" without rel="noreferrer"',()=>
-    expect('foo').to.equal('bar')
+    underTest.lintFile(getFixture('page-without-noreferrer.html'))
+      .then(lintResult => expect(lintResult).to.eql([
+        expectedResult('https://this.domain/resource', 'nofollow'),
+        expectedResult('http://remote.com/resource', 'nofollow'),
+        expectedResult('/resource'),
+        expectedResult('https://this.domain/resource', ''),
+        expectedResult('https://this.domain/resource', 'nofollow'),
+        expectedResult('https://remote.com/resource', ''),
+        expectedResult('http://remote.com/resource', 'nofollow'),
+      ]))
   );
   it('should identify links where target="_blank" without rel="noopener"',()=>
-    expect('foo').to.equal('bar')
-  );
-  it(`should issue warning for links where target="_blank" without either rel="noopener/noreferrer"
-for links to same domain`,()=>
-    expect('foo').to.equal('bar')
+    underTest.lintFile(getFixture('page-without-noopener.html'))
+      .then(lintResult => expect(lintResult).to.eql([
+        expectedResult('https://this.domain/resource', 'noreferrer'),
+        expectedResult('http://remote.com/resource', 'noreferrer'),
+        expectedResult('/resource'),
+        expectedResult('https://this.domain/resource', ''),
+        expectedResult('https://this.domain/resource', 'noreferrer'),
+        expectedResult('https://remote.com/resource', ''),
+        expectedResult('http://remote.com/resource', 'noreferrer'),
+      ]))
   );
   it('should allow links where target="_blank" with rel="noopener noreferrer"',()=>
-    expect('foo').to.equal('bar')
+    underTest.lintFile(getFixture('page-with-valid-target-blank.html'))
+      .then(lintResult => expect(lintResult).to.eql([]))
   );
 });
